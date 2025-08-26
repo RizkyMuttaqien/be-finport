0c8b62461e46ec76d2610883da580d19e32586dd
muhammad rizky muttaqien <rizky.muttaqien1@gmail.com>
2025-08-26 09:28:21 +0700

    check deploy

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const routes = require("./routes");
 const app = express();
 app.use(express.json());
 app.use("/api", routes);
-
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
