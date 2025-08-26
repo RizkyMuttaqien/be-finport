1d2bd4d3138c26a28904ca6951e319a70cbff404
muhammad rizky muttaqien <rizky.muttaqien1@gmail.com>
2025-08-26 10:06:27 +0700

    update mysql2

--- app.js
+++ app.js
@@ -8,5 +8,4 @@ app.use("/api", routes);
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
