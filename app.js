83c4d18efb2ff28a864dd0a2101a48c3d57fb542
muhammad rizky muttaqien <rizky.muttaqien1@gmail.com>
2025-08-29 14:29:54 +0700

    routes vercel

--- app.js
+++ app.js
@@ -2,14 +2,8 @@
 const express = require("express");
 const routes = require("./routes");
 const path = require("path");
-const cors = require("cors");
 const app = express();
 
-app.use(cors({
-    origin: "https://fe-finport.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true,
-}));
 
 app.use(express.json());
 app.use("/api", routes);
