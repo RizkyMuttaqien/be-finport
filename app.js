581eafe9471c0301f3b4a8aba2ccd7a32832dfdf
muhammad rizky muttaqien <rizky.muttaqien1@gmail.com>
2025-08-29 14:18:09 +0700

    add cors

--- app.js
+++ app.js
@@ -2,8 +2,16 @@
 const express = require("express");
 const routes = require("./routes");
 const path = require("path");
+const cors = require("cors");
 const app = express();
 
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+app.options("*", cors());
 app.use(express.json());
 app.use("/api", routes);
 app.get("/", (req, res) => {
