7722ceb55a23e1498942ad2817e4cf30e414ef9f
muhammad rizky muttaqien <rizky.muttaqien1@gmail.com>
2025-08-29 13:31:27 +0700

    add cors

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-    origin: "https://be-finport.vercel.app",
+    origin: "https://fe-finport.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
 }));
